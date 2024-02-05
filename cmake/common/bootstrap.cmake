a927de88ece602b52d109a63a219f1cb233748a5
derrod <xlnedder@gmail.com>
2024-02-05 05:44:35 +0100

    CI/cmake: Strip out macOS/Linux and setup basics

--- cmake/common/bootstrap.cmake
+++ cmake/common/bootstrap.cmake
@@ -48,7 +48,6 @@ string(JSON _website GET ${buildspec} website)
 string(JSON _author GET ${buildspec} author)
 string(JSON _email GET ${buildspec} email)
 string(JSON _version GET ${buildspec} version)
-string(JSON _bundleId GET ${buildspec} platformConfig macos bundleId)
 string(JSON _windowsAppUUID GET ${buildspec} uuids windowsApp)
 # cmake-format: on
 
@@ -56,7 +55,6 @@ set(PLUGIN_AUTHOR ${_author})
 set(PLUGIN_WEBSITE ${_website})
 set(PLUGIN_EMAIL ${_email})
 set(PLUGIN_VERSION ${_version})
-set(MACOS_BUNDLEID ${_bundleId})
 
 include(buildnumber)
 include(osconfig)
